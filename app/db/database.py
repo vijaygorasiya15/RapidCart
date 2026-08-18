a1feeca9d0122e476e9da0ebd30bd05657637418
vijaygorasiya15 <vijaygorasiya15@gmail.com>
2026-08-18 13:54:14 +0530

    feat: project structure + database connection setup

---
+++ app/db/database.py
@@ -0,0 +1,16 @@
+from app.core.config import settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+engine = create_engine(settings.DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
